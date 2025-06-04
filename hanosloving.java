5ed29b0b84c1531bd59790bff47051735496b7c2
koyyaguraniharika <Niharikakoyyagura@gmail.com>
2025-06-04 11:33:19 +0530

    Add files via upload

---
+++ hanosloving.java
@@ -0,0 +1,16 @@
+public class main{
+public static void slove(intn,charsource,charauxiliary,char destination){
+if(n==1){
+system.out.println('movedisk1from"+source+"to"+destination);
+return;
+}
+sloveHano(n-1,source destination,auxiliary);
+System.out.println("movedisk"+"from+source+"t0"+destination);
+sloveHano(n-1,auxilary,source,destination);
+}
+public static void main{string[]args){
+int num()
+system.out.println("towers to Hano;solution for"+numdisks+"disks:");
+solveHano:(num Disks,'A','B','C');
+}
+}
